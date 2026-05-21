c0f23d27678fd820d580214a4a6cc5d85fd0f78b
aregardi <alexis.regardin@student.unamur.be>
2026-05-21 14:39:27 +0200

    corrected errors

--- CacheSimulator/src/response.py
+++ CacheSimulator/src/response.py
@@ -1,36 +1,42 @@
 class Response:
-    """Container representing a cache response.
-
-    Attributes:
-        hit_list (Dict[str, bool]): Mapping of component/actor names to hit status.
-        time (Union[int, float]): Cumulative time cost associated with the response.
-        actor (str): Identifier of the actor producing the response.
-        data (str): Optional payload or metadata.
-    """
-
-    def __init__(self, hit_list: Dict[str, bool], time: Union[int, float], actor: str = "UNKNOWN", data: str = "") -> None:
+    def __init__(self, hit_list, time, actor="UNKNOWN", data=""):
         """Initialize a Response instance.
 
-        Args:
-            hit_list: Mapping of names to boolean hit statuses.
-            time: Initial cumulative time value.
-            actor: Optional actor identifier (default "UNKNOWN").
-            data: Optional data payload (default empty string).
+        Parameters
+            hit_list 
+                dict
+                Mapping from actor name to boolean indicating hit status.
+            time
+                int or float
+                Initial time/cost for the response.
+            actor
+                str, optional
+                Identifier of the actor producing the response (default "UNKNOWN").
+            data
+                any, optional
+                Optional payload attached to the response (default empty string).
         """
         self.hit_list = hit_list
         self.time = time
         self.actor = actor
         self.data = data
 
-    def deepen(self, time: Union[int, float], name: str) -> None:
-        """Mark a named entry as a miss and increase the cumulative time.
+    def deepen(self, time, name):
+        """Apply additional time cost and mark an actor as not hit.
 
-        The named entry in hit_list will be set to False and the provided time
-        value will be added to the Response's cumulative time.
+        This method sets hit_list[name] to False and increments the response's
+        total time by the provided amount.
 
-        Args:
-            time: Time to add to the cumulative time.
-            name: Key in hit_list to mark as False.
+        Parameters
+            time
+                int or float
+                Amount of time to add to the response's total time.
+            name
+                str
+                Actor name whose hit status will be changed to False.
+
+        Returns
+            None
         """
         self.hit_list[name] = False
         self.time += time
